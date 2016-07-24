fc3b2fdc065635c19cc0049e0716fabbad7da494
kurttomlinson <kurttomlinson@gmail.com>
2016-07-24 15:56:36 -0500

    start up 100 nodes at a time instead of just 1

--- crawlDHT.js
+++ crawlDHT.js
@@ -1,3 +1,5 @@
+var number_of_nodes = 100;
+
 var config = require('./config/database');
 var util = require('util');
 
@@ -15,27 +17,39 @@ console.log = function(data) {
 
 var DHT = require('bittorrent-dht');
 
-var dht = new DHT();
-  
-dht.listen(6881, function(){
-  console.log('now listening');
-  console.log(dht.address());
-});
+function make_node(node_index){
+  var build = function() {
+    dht = new DHT();
+    dht.listen(16881 + node_index, function(){
+      console.log('now listening: '+node_index);
+      //console.log(dht_array[i].address());
+    });
 
-dht.on('ready',function() {
-  console.log('now ready');
-});
+    dht.on('ready',function() {
+      console.log('now ready: '+node_index);
+    });
 
-dht.on('announce', function(peer, infoHash) {
-  console.log("announce : "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
-  dht.lookup(infoHash);
-  client.rpush("DHTS", infoHash.toString('hex'));
-});
+    dht.on('announce', function(peer, infoHash) {
+      console.log("announce "+node_index+": "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
+      dht.lookup(infoHash);
+      client.rpush("DHTS", infoHash.toString('hex'));
+    });
 
-dht.on('peer', function(peer, infoHash, from) {
-  console.log("peer : "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
-});
+    dht.on('peer', function(peer, infoHash, from) {
+      //console.log("peer : "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
+    });
 
-dht.on('error',function(err) {
-  dht.destroy();
-});
+    dht.on('error',function(err) {
+      dht.destroy();
+    });
+  }
+  return build;
+}
+
+
+
+var nodes = new Array();
+for (i=0; i<number_of_nodes; i++) {
+  nodes[i] = make_node(i);
+  nodes[i]();
+}
