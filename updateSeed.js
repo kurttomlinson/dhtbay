552658e4a75af852e2fec95c82cf4a61040d73e8
kurttomlinson <kurttomlinson@gmail.com>
2016-07-24 20:24:12 -0500

    process swarm data more accurately

--- updateSeed.js
+++ updateSeed.js
@@ -26,26 +26,46 @@ lastDay.setDate(lastDay.getDate() - 1);
 var filter = {};
 var limit_count = 1000;
 
+var seconds_per_torrent = 2;
+
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+};
+
 var stream = Torrent.find(filter).sort({'lastmod': -1}).limit(limit_count).stream();
 stream.on('data', function(torrent) {
 	var self = this;
 	self.pause();
+	setTimeout(function(){
+                var seeders_max = Math.max.apply(null, seeders);
+                var leechers_max = Math.max.apply(null, leechers);
+		torrent.swarm.seeders = seeders_max;
+                torrent.swarm.leechers = leechers_max;
+                torrent.lastmod = new Date();
+
+		client.destroy();
+
+                torrent.save(function(err) {
+                        if(err) { console.log("Error while saving"+err); }
+                        console.log("Torrent saved: "+torrent._id + " Seeders: " + torrent.swarm.seeders + " Leechers: " + torrent.swarm.leechers);
+                });
+
+		self.resume();
+	}, seconds_per_torrent * 1000);
 	console.log("========================================================================================");
 	console.log("Processing torrent : " + torrent._id);
 	var parsedTorrent = { 'infoHash': torrent._id, 'length': torrent.size, 'announce': torrent.details };
 
 	var client = Client(peerId, port, parsedTorrent);
+	var seeders = new Array();
+        var leechers = new Array();
 	client.on('scrape', function(data) {
 		console.log("got a response from tracker: "+data.announce);
 		console.log("number of seeders : "+data.complete);
 		console.log("number of leechers : "+data.incomplete);
-		torrent.swarm.seeders = data.complete;
-		torrent.swarm.leechers = data.incomplete;
-		torrent.lastmod = new Date();
-		torrent.save(function(err) {
-			if(err) { console.log("Error while saving"+err); }
-			console.log("Torrent saved: "+torrent._id + " Seeds: " + torrent.swarm.seeders); self.resume();
-		});
+
+		seeders.push(data.complete);
+		leechers.push(data.incomplete);
 	});
 	client.on('error', function(err) {
 		console.log("Torrent client error : "+err); //self.resume();
