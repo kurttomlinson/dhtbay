582333ad43782301fc2c5b315abb858601397f7e
kurttomlinson <kurttomlinson@gmail.com>
2016-07-24 19:23:28 -0500

    fix concurrent nodes so they actually use different ports

--- crawlDHT.js
+++ crawlDHT.js
@@ -1,4 +1,4 @@
-var number_of_nodes = 1000;
+var number_of_nodes = 100;
 
 var config = require('./config/database');
 var util = require('util');
@@ -18,19 +18,20 @@ console.log = function(data) {
 var DHT = require('bittorrent-dht');
 
 function make_node(node_index){
+  var port = 16881 + node_index
+  var dht = new DHT();
   var build = function() {
-    dht = new DHT();
-    dht.listen(16881 + node_index, function(){
-      console.log('now listening: '+node_index);
-      //console.log(dht_array[i].address());
+    dht.listen(port, function(){
+      console.log('now listening: ' + port);
     });
 
     dht.on('ready',function() {
-      console.log('now ready: '+node_index);
+      console.log("address " + port + ": " + JSON.stringify(dht.address()));
+      console.log('now ready: ' + port);
     });
 
     dht.on('announce', function(peer, infoHash) {
-      console.log("announce "+node_index+": "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
+      console.log("announce "+port+": "+peer.host + ':' + peer.port+' : '+infoHash.toString('hex'));
       dht.lookup(infoHash);
       client.rpush("DHTS", infoHash.toString('hex'));
     });
