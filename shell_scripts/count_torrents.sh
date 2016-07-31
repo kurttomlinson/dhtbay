e3ab95191febf244305e10d5da4313a979d2b24d
kurttomlinson <kurttomlinson@gmail.com>
2016-07-31 15:39:08 -0500

    fix counting script output

--- shell_scripts/count_torrents.sh
+++ shell_scripts/count_torrents.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo `date "+%Y-%M-%d   %H:%M:%S"`      `ls -l /home/dht/dhtbay/torrent/ | wc -l` >> /home/dht/dhtbay/logs/count.txt
+printf "`date "+%Y-%M-%d   %H:%M:%S"`   `ls -l /home/dht/dhtbay/torrent/ | wc -l`\n"
